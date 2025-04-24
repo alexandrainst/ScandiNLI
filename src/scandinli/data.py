bfcccc87c3de93cc8a294cebd6a68e99bc779382
saattrupdan <saattrupdan@gmail.com>
2025-04-24 16:35:29 +0200

    debug

--- src/scandinli/data.py
+++ src/scandinli/data.py
@@ -244,6 +244,7 @@ def build_dataset_for_single_language(
             all_datasets.append(dataset)
 
     # Concatenate the datasets
+    breakpoint()
     dataset = concatenate_datasets(all_datasets).shuffle(seed=seed)
 
     # Return the dataset
